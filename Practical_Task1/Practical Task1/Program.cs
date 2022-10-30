5f648acdb59a50046d202c6c7218e80827d3bfa9
Zakaria <elbakidze.zaqaria@gmail.com>
2022-10-30 18:12:33 +0400

    Development and build tools task

---
+++ Practical_Task1/Practical Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+
+public class Task1
+{
+    public static void Read(string input, int len)
+    {
+
+        char characters;
+        char[] charArray = input.ToCharArray();
+        string s = new string(charArray);
+        //return s;
+        int count = 0;
+        for(int i = 1; i < s.Length; i++)
+        {
+            count += 1;
+            if (!s[i - 1].Equals(charArray[i]))
+                Console.WriteLine($"{s[i]} {count + 1}");
+            else
+            {
+                Console.WriteLine("~");
+            } 
+            
+        }
+        
+    }
+    public static void Main()
+    {
+        Console.WriteLine("repeated values will be replaced by \"~\"");
+        String str = Console.ReadLine();
+        int n = str.Length;
+        Console.WriteLine("\ninput results: ");
+        Console.WriteLine($"{str[0]} 1");
+        Read(str, n);
+    }
+}
